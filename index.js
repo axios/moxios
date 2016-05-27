8361767a2dc4fe527d448439e055c241cbd5b668
Matt Zabriskie <mzabriskie@gmail.com>
2016-05-27 14:51:14 -0600

    A bit of class cleanup

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import btoa from 'axios/lib/helpers/btoa'
 import cookies from 'axios/lib/helpers/cookies'
 import settle from 'axios/lib/helpers/settle'
 
+const DEFAULT_WAIT_DELAY = 100
+
 // The default adapter
 let defaultAdapter
 
@@ -31,94 +33,60 @@ let mockAdapter = (resolve, reject, config) => {
   }
 }
 
-class Moxios {
-  constructor(delay = 100) {
-    this.stubs = new Tracker()
-    this.requests = new Tracker()
-    this.delay = delay
-  }
-
-  /**
-   * Install the mock adapter for axios
-   */
-  install(instance = axios) {
-    defaultAdapter = instance.defaults.adapter
-    instance.defaults.adapter = mockAdapter
-  }
-
-  /**
-  * Uninstall the mock adapter and reset state
-  */
-  uninstall(instance = axios) {
-    instance.defaults.adapter = defaultAdapter
-    this.stubs.reset()
-    this.requests.reset()
-  }
-
-  /**
-  * Stub a response to be used to respond to a request matching a URL or RegExp
-  *
-  * @param {String|RegExp} urlOrRegExp A URL or RegExp to test against
-  * @param {Object} response The response to use when a match is made
-  */
-  stubRequest(urlOrRegExp, response) {
-    this.stubs.track({url: urlOrRegExp, response})
-  }
-
-  /**
-  * Run a single test with mock adapter installed.
-  * This will install the mock adapter, execute the function provided,
-  * then uninstall the mock adapter once complete.
-  *
-  * @param {Function} fn The function to be executed
-  */
-  withMock(fn) {
-    this.install()
-    try {
-      fn()
-    } finally {
-      this.uninstall()
-    }
-  }
-
-  /**
-  * Wait for request to be made before proceding.
-  * This is naively using a `setTimeout`.
-  * May need to beef this up a bit in the future.
-  *
-  * @param {Function} fn The function to execute once waiting is over
-  * @param {Number} delay How much time in milliseconds to wait
-  */
-  wait(fn, delay = this.delay) {
-    setTimeout(fn, delay)
-  }
-}
-
 class Tracker {
   constructor() {
     this.__items = []
   }
 
+  /**
+   * Reset all the items being tracked
+   */
   reset() {
     this.__items.splice(0)
   }
 
+  /**
+   * Add an item to be tracked
+   *
+   * @param {Object} item An item to be tracked
+   */
   track(item) {
     this.__items.push(item)
   }
   
+  /**
+   * The count of items being tracked
+   *
+   * @return {Number}
+   */
   count() {
     return this.__items.length
   }
 
+  /**
+   * Get an item being tracked at a given index
+   *
+   * @param {Number} index The index for the item to retrieve
+   * @return {Object}
+   */
   at(index) {
     return this.__items[index]
   }
   
+  /**
+   * Get the first item being tracked
+   *
+   * @return {Object}
+   */
   first() {
     return this.at(0)
   }
 
+  /**
+   * Get the most recent (last) item being tracked
+   *
+   * @return {Object}
+   */
   mostRecent() {
     return this.at(this.count() - 1)
   }
@@ -169,21 +137,8 @@ class Request {
    * @return {Promise} A Promise that resolves once the response is ready
    */
   respondWith(res) {
-    let response = {
-      data: transformData(
-        res.responseText || res.response,
-        res.headers,
-        this.config.transformResponse
-      ),
-      status: res.status,
-      statusText: res.statusText,
-      headers: res.headers,
-      config: this.config,
-      request: this
-    }
-
+    let response = new Response(this, res)
     settle(this.resolve, this.reject, response)
-
     return new Promise(function (resolve) {
       moxios.wait(function () {
         resolve(response)
@@ -192,5 +147,87 @@ class Request {
   }
 }
 
-let moxios = new Moxios()
+class Response {
+  /**
+   * Create a new Response object
+   *
+   * @param {Request} req The Request that this Response is associated with
+   * @param {Object} res The data representing the result of the request
+   */
+  constructor(req, res) {
+    this.config = req.config
+    this.data = transformData(
+      res.responseText || res.response,
+      res.headers,
+      this.config.transformResponse
+    )
+    this.status = res.status
+    this.statusText = res.statusText
+    this.headers = res.headers
+    this.request = req
+  }
+}
+
+let moxios = {
+  stubs: new Tracker(),
+  requests: new Tracker(),
+  delay: DEFAULT_WAIT_DELAY,
+
+  /**
+   * Install the mock adapter for axios
+   */
+  install: function(instance = axios) {
+    defaultAdapter = instance.defaults.adapter
+    instance.defaults.adapter = mockAdapter
+  },
+
+  /**
+  * Uninstall the mock adapter and reset state
+  */
+  uninstall: function(instance = axios) {
+    instance.defaults.adapter = defaultAdapter
+    this.stubs.reset()
+    this.requests.reset()
+  },
+
+  /**
+  * Stub a response to be used to respond to a request matching a URL or RegExp
+  *
+  * @param {String|RegExp} urlOrRegExp A URL or RegExp to test against
+  * @param {Object} response The response to use when a match is made
+  */
+  stubRequest: function(urlOrRegExp, response) {
+    this.stubs.track({url: urlOrRegExp, response})
+  },
+
+  /**
+  * Run a single test with mock adapter installed.
+  * This will install the mock adapter, execute the function provided,
+  * then uninstall the mock adapter once complete.
+  *
+  * @param {Function} fn The function to be executed
+  */
+  withMock: function(fn) {
+    this.install()
+    try {
+      fn()
+    } finally {
+      this.uninstall()
+    }
+  },
+
+  /**
+  * Wait for request to be made before proceding.
+  * This is naively using a `setTimeout`.
+  * May need to beef this up a bit in the future.
+  *
+  * @param {Function} fn The function to execute once waiting is over
+  * @param {Number} delay How much time in milliseconds to wait
+  */
+  wait: function(fn, delay = this.delay) {
+    setTimeout(fn, delay)
+  }
+}
+
+
 export default moxios
